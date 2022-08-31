041530acc082866f496c38241ece6101882ff7ec
Nihal <nihal@nevercode.io>
2022-08-31 13:22:56 +0300

    Initial Commit

---
+++ Shared/native_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_ios_appApp.swift
+//  Shared
+//
+//  Created by Niall on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct native_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
