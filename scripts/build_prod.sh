10b1f8956d20316e0ee160601fc4af57874cb0f9
Nihal <nihalagazade@gmail.com>
2022-10-12 09:35:19 +0300

    workflowid

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,7 +1,7 @@
 curl -H "Content-Type: application/json" -H "x-auth-token: UjW3noRBwGnyBlOOfHhc8suwuNZmhT4XP7SaeXV-Z7c" \
 --data '{
     "appId": "630f398f2923d88662031273", 
-    "workflowId": "ios-release",
+    "workflowId": "ios_release",
     "branch": "main"
 }' \
 https://api.codemagic.io/builds
